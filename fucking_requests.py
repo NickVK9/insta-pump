684dafcca08116f7f7bd5a80a2170f1b6d18d186
Base <nikitkakorepanov2014@yandex.ru>
2020-06-06 18:43:40 +0200

    kazakchstan

--- fucking_requests.py
+++ fucking_requests.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 
 ask = requests.get('https://www.instagram.com/korepanov_nv/?__a=1')
 soup = BeautifulSoup(ask.content, 'lxml')
-body = soup.find("div", id="bodyContent")
+#body = soup.find("div", id="bodyContent")
 
-print(body)
+print(soup)
 
 # web: python instapump_testbot.py
