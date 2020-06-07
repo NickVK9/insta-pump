40bae02a1d586f4260623a7b5c58b9f5faab332d
Base <nikitkakorepanov2014@yandex.ru>
2020-06-07 11:00:05 +0200

    one more try to hack

--- fuuuuuck.py
+++ fuuuuuck.py
@@ -1,6 +1,27 @@
-from bs4 import BeautifulSoup
-import requests
+import telebot
+from bs4 import BeautifulSoup as bs
+import requests as req
+import json
+from flask import Flask, request
+import os
 
-ask = requests.get('https://www.instagram.com/korepanov_nv/')
-print(ask.content)
-soup = BeautifulSoup(ask.content, 'html.parser')
+def scrape_data(user):
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}
+    ask = req.get('https://www.instagram.com/{}'.format(user))
+    print(ask.headers)
+    print('')
+    print(ask.history)
+    print('')
+    print(ask.cookies)
+
+    soup = bs(ask.text, 'html.parser')
+    body = soup.find('body')
+    script = body.find('script', text = lambda t: t.startswith('window._sharedData'))
+
+    page_json = script.text.split(' = ', 1)[1].rstrip(';')
+    data_json = json.loads(page_json)
+    print('')
+    print(data_json)
+
+
+    return data_json
