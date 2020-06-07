a1a5b5a2a041f40804da06600f619c3b4de4304d
Base <nikitkakorepanov2014@yandex.ru>
2020-06-07 11:15:43 +0200

    one more try to hack

--- fuuuuuck.py
+++ fuuuuuck.py
@@ -7,7 +7,8 @@ import os
 
 def scrape_data(user):
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}
-    ask = req.get('https://www.instagram.com/{}'.format(user))
+    cookies = {}
+    ask = req.get('https://www.instagram.com/{}'.format(user), cookies=cookies)
     print(ask.headers)
     print('')
     print(ask.history)
