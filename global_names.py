e4fff65d5a7fc30d00e4726089cd0bc6a557432c
Daniil <49916703+sicutglacies@users.noreply.github.com>
2020-06-10 18:18:43 +0500

    Update global_names.py

--- global_names.py
+++ global_names.py
@@ -10,9 +10,12 @@ CHROME_WIN_UA = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML,
 # #Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36
 # #Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36
 # #Instagram 123.0.0.21.114 (iPhone; CPU iPhone OS 11_4 like Mac OS X; en_US; en-US; scale=2.00; 750x1334) AppleWebKit/605.1.15
-LOGIN = 'instapump_support'  # секретные данные акка
-PASSWORD = 'wetryingtodothisabout1week'
-PHONE_NUMBER = '+79651139899'
+LOGIN = 'support_me_pls'  # секретные данные акка
+PASSWORD = 'ihatemark33'
+PHONE_NUMBER = '+380916072406'
+# LOGIN = 'instapump_support'  # секретные данные акка
+# PASSWORD = 'wetryingtodothisabout1week'
+# PHONE_NUMBER = '+79651139899'
 
 TEST_TOKEN = '966681948:AAGh0kkPeXK1CtEsqDAQEjVAFDF6sYuniew'
 TOKEN = '1124156274:AAFcflDj26OJnIcucf70mi7IlNdikylfGIw'
