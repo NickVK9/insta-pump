3c845dd82151e4ea6036a7a7392798b7fb511942
Base <nikitkakorepanov2014@yandex.ru>
2020-06-13 19:26:13 +0200

    God, please

--- instapump_testbot.py
+++ instapump_testbot.py
@@ -104,17 +104,13 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Введи инстаграм логин друга:')
         bot.register_next_step_handler(message, data_from_instagram.friends_rating)
     elif message.text == 'тест':
-        try:
-            r = requests.get('https://www.instagram.com/korepanov_nv/')
-            soup = bs(r.content)
-            scripts = soup.find_all('script', type="text/javascript", text=re.compile('window._sharedData'))
-            stringified_json = scripts[0].get_text().replace('window._sharedData = ', '')[:-1]
+        r = requests.get('https://www.instagram.com/korepanov_nv/')
+        soup = bs(r.content)
+        scripts = soup.find_all('script', type="text/javascript", text=re.compile('window._sharedData'))
+        stringified_json = scripts[0].get_text().replace('window._sharedData = ', '')[:-1]
 
-            datas = json.loads(stringified_json)['entry_data']['ProfilePage'][0]
-            bot.send_message(message.chat.id, datas['graphql']['user']['biography'])
-        except Exception as e:
-            bot.send_message(message.chat.id, 'Сори не вышло')
-            bot.send_message(message.chat.id, str(e))
+        datas = json.loads(stringified_json)['entry_data']['ProfilePage'][0]
+        bot.send_message(message.chat.id, datas['graphql']['user']['biography'])
     else:
         bot.send_message(message.chat.id, 'Используй кнопки!')
 
