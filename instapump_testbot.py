54246539f504d2e69acb9e437ac5a16672b3acb6
Base <nikitkakorepanov2014@yandex.ru>
2020-06-13 19:29:38 +0200

    God, please

--- instapump_testbot.py
+++ instapump_testbot.py
@@ -105,12 +105,12 @@ def send_text(message):
         bot.register_next_step_handler(message, data_from_instagram.friends_rating)
     elif message.text == 'тест':
         r = requests.get('https://www.instagram.com/korepanov_nv/')
-        soup = bs(r.content)
+        soup = bs(r.content, 'lxml')
         scripts = soup.find_all('script', type="text/javascript", text=re.compile('window._sharedData'))
         stringified_json = scripts[0].get_text().replace('window._sharedData = ', '')[:-1]
-
-        datas = json.loads(stringified_json)['entry_data']['ProfilePage'][0]
-        bot.send_message(message.chat.id, datas['graphql']['user']['biography'])
+        print(stringified_json)
+        #datas = json.loads(stringified_json)['entry_data']['ProfilePage'][0]
+        #bot.send_message(message.chat.id, datas['graphql']['user']['biography'])
     else:
         bot.send_message(message.chat.id, 'Используй кнопки!')
 
