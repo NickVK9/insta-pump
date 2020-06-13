29a2fefb5e2e697fb2eb2255bcfd0372ed998443
Daniil <49916703+sicutglacies@users.noreply.github.com>
2020-06-13 23:02:18 +0500

    Update fuck_zuck.py

--- fuck_zuck.py
+++ fuck_zuck.py
@@ -55,4 +55,5 @@ soup = BeautifulSoup(r.content, 'lxml')
 scripts = soup.find_all('script', type="text/javascript", text=re.compile('window._sharedData'))
 stringified_json = scripts[0].get_text().replace('window._sharedData = ', '')[:-1]
 
+print(stringified_json)
 print(json.loads(stringified_json)['entry_data']['ProfilePage'][0])
